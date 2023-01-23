d19d7f54568e7d386c1cfa6280eeaa4a8453fd81
brunodev <brunpdev@timudev.cl>
2023-01-23 15:12:08 -0300

    ADD

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Slider } from '@mui/material';
+import MaterialReactTable from 'material-react-table';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState([]);
+	const [isError, setIsError] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [rowCount, setRowCount] = useState(0);
+
+	//table state
+	const [columnFilters, setColumnFilters] = useState([]);
+	const [globalFilter, setGlobalFilter] = useState('');
+	const [sorting, setSorting] = useState([]);
+	const [pagination, setPagination] = useState({
+		pageIndex: 0,
+		pageSize: 10,
+	});
+
+	useEffect(() => {
+		const fetchData = async () => {
+			setIsLoading(true);
+			setIsError(null);
+
+			try {
+				const response = await axios.get(
+					'https://api.instantwebtools.net/v1/passenger?page=0&size=10'
+				);
+				const data = response.data;
+				setData(data);
+			} catch (error) {
+				console.error(error);
+				setIsError(error);
+			}
+
+			setIsLoading(false);
+		};
+
+		fetchData();
+	}, []);
+
+	if (isLoading) {
+		return <p>Cargando...</p>;
+	}
+
+	if (isError) {
+		return <p>Error: {isError.message}</p>;
+	}
+
+	console.log('--->La Data:', data);
+
+	return <div></div>;
 }
 
 export default App;
